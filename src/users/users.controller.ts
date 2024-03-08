95ebfbf9f383656e2d79f98c99d417991cd090a3
anderskambenders <a.zetsmanis@mail.ru>
2024-03-09 01:56:08 +0300

    feat: remove password from user response, set delete code 204

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,13 +3,16 @@ import {
   Controller,
   Delete,
   Get,
+  Header,
+  HttpCode,
+  HttpStatus,
   Param,
   ParseUUIDPipe,
   Post,
   Put,
 } from '@nestjs/common';
 import UsersService from './users.service';
-import IUser from './types/users.interface';
+import { UserResponse } from './entities/users.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-user.dto';
 
@@ -18,28 +21,39 @@ class UsersController {
   constructor(private usersService: UsersService) {}
   @Get()
   async findUsers() {
-    return this.usersService.getAllUsers();
+    return this.usersService
+      .getAllUsers()
+      .map((user) => new UserResponse(user));
   }
   @Get(':id')
-  async findUser(@Param('id', ParseUUIDPipe) id: string): Promise<IUser> {
-    return this.usersService.getUserById(id);
+  async findUser(
+    @Param('id', ParseUUIDPipe) id: string,
+  ): Promise<UserResponse> {
+    return new UserResponse(await this.usersService.getUserById(id));
   }
 
   @Post()
-  async createUser(@Body() createUserDto: CreateUserDto): Promise<IUser> {
-    return await this.usersService.createUser(createUserDto);
+  @Header('content-type', 'application/json')
+  @HttpCode(HttpStatus.CREATED)
+  @Header('content-type', 'application/json')
+  async createUser(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<UserResponse> {
+    return new UserResponse(await this.usersService.createUser(createUserDto));
   }
 
   @Put(':id')
   async updateUser(
     @Body() updatePasswordDto: UpdatePasswordDto,
     @Param('id', ParseUUIDPipe) id: string,
-  ): Promise<IUser> {
-    return this.usersService.updateUserPassword(id, updatePasswordDto);
+  ): Promise<UserResponse> {
+    return new UserResponse(
+      await this.usersService.updateUserPassword(id, updatePasswordDto),
+    );
   }
-
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
-  async deleteUser(@Param('id', ParseUUIDPipe) id: string): Promise<void> {
+  async deleteUser(@Param('id', ParseUUIDPipe) id: string) {
     return this.usersService.deleteUser(id);
   }
 }
